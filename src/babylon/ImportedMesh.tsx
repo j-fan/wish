bee56cb5af1fd0cb4548ab8a5a26cb3fcea37161
jfan <jfan9988@gmail.com>
2021-05-29 16:41:19 +1000

    create ImportedMesh component

---
+++ src/babylon/ImportedMesh.tsx
@@ -0,0 +1,65 @@
+import { AbstractMesh, SceneLoader, Vector3 } from "@babylonjs/core";
+import React, { FunctionComponent, Suspense, useEffect, useState } from "react";
+import { useScene } from "react-babylonjs";
+import "@babylonjs/loaders/glTF";
+import { v4 as uuidv4 } from "uuid";
+
+export type MeshProperties = {
+  position?: Vector3;
+  scaling?: Vector3;
+  rotation?: Vector3;
+};
+
+type ImportedMeshProps = {
+  rootUrl: string;
+  sceneFilename?: string;
+} & MeshProperties;
+
+const ImportedMesh: FunctionComponent<ImportedMeshProps> = ({
+  position = Vector3.Zero(),
+  scaling = new Vector3(1, 1, 1),
+  rotation = Vector3.Zero(),
+  rootUrl,
+  sceneFilename,
+}) => {
+  const scene = useScene();
+  const [mesh, setMesh] = useState<AbstractMesh>();
+
+  useEffect(() => {
+    if (mesh) {
+      return;
+    }
+
+    SceneLoader.ImportMesh(
+      "",
+      rootUrl,
+      sceneFilename,
+      scene,
+      (loadedMesh, _particleSystems, _skeletons, animationGroups) => {
+        animationGroups?.forEach((animation) => {
+          animation.start(true);
+        });
+
+        setMesh(loadedMesh[0]);
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    if (!mesh) {
+      return;
+    }
+
+    mesh.position = position;
+    mesh.scaling = scaling;
+    mesh.rotation = rotation;
+  }, [position, rotation, scaling, mesh]);
+
+  return (
+    <Suspense fallback={<box name="fallback" scaling={Vector3.Zero()} />}>
+      <mesh name={`${sceneFilename}-${uuidv4()}`} fromInstance={mesh} />
+    </Suspense>
+  );
+};
+
+export { ImportedMesh };
--- src/babylon/StarModel.tsx
+++ src/babylon/StarModel.tsx
@@ -1,39 +1,19 @@
-import { Scene, Vector3 } from "@babylonjs/core";
-import React, { FunctionComponent, Suspense } from "react";
-import { ILoadedModel, Model } from "react-babylonjs";
+import { Vector3 } from "@babylonjs/core";
+import React, { FunctionComponent } from "react";
 import "@babylonjs/loaders/glTF";
-import { v4 as uuidv4 } from "uuid";
+import { ImportedMesh, MeshProperties } from "./ImportedMesh";
 
-type StarModelProps = {
-  scene: Scene;
-  position?: Vector3;
-  scaleToDimension?: number;
-  rotation?: Vector3;
-};
-
-const StarModel: FunctionComponent<StarModelProps> = ({
-  position = Vector3.Zero(),
-  scaleToDimension = 1,
+const StarModel: FunctionComponent<MeshProperties> = ({
+  scaling = new Vector3(40, 40, 40),
   ...props
 }) => {
-  const onLoaded = (model: ILoadedModel) => {
-    model.animationGroups?.forEach((animation) => {
-      animation.start(true);
-    });
-  };
-
   return (
-    <Suspense fallback={<box name="fallback" scaling={Vector3.Zero()} />}>
-      <Model
-        name={`star-model-${uuidv4()}`}
-        rootUrl="models/"
-        sceneFilename="star.glb"
-        position={position}
-        scaleToDimension={scaleToDimension}
-        onModelLoaded={onLoaded}
-        {...props}
-      />
-    </Suspense>
+    <ImportedMesh
+      rootUrl="models/"
+      sceneFilename="star.glb"
+      scaling={scaling}
+      {...props}
+    />
   );
 };
 
